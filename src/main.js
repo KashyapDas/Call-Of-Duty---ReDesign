5c804eb03339d4385cbb12360d20e7ae66e9349a
Kashyap Das <kashyap.education2234@gmail.com>
2025-07-02 12:38:46 +0530

    writing the logic of page transition effect append in the navbar links

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
-console.log("ReDesign Call of Duty website")
+const links = document.querySelectorAll(".link");
+const mainContent = document.querySelector("#contentPage");
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    // first time render the Home page
+    mainContent.innerHTML = `<div id="hero">
+        <h1>Home</h1>
+      </div>`
+})
+
+links.forEach((tab)=>{
+    tab.addEventListener("click",async (dets)=>{
+
+        // Add the gsap animation start here and await it 
+
+        if(tab.textContent === "Home"){
+            mainContent.innerHTML = `<div id="hero">
+        <h1>Home</h1>
+      </div>`
+        }
+        else if(tab.textContent === "About"){
+            mainContent.innerHTML = `<div id="hero">
+        <h1>About</h1>
+      </div>`
+        }
+        else if(tab.textContent === "Contact"){
+            mainContent.innerHTML = `<div id="hero">
+        <h1>Contact</h1>
+      </div>`
+        }
+        else{
+            // dynamic creation of not found page...and push to the html (not implemented yet)
+        }
+
+        // Add the gsap animation of end here and await it
+        
+    })
+})
