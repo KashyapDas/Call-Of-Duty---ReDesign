8f0602ab33691f35b14b6c0c7170b2733b084a6c
Kashyap Das <kashyap.education2234@gmail.com>
2025-07-02 17:57:54 +0530

    create a navbar and add page transition effect on each link clicks. Also implement the features of single page features

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const links = document.querySelectorAll(".link");
 const mainContent = document.querySelector("#contentPage");
-
-
+// Default page loaded on the website load for the 1st time
 document.addEventListener("DOMContentLoaded",()=>{
     // first time render the Home page
     mainContent.innerHTML = `<div id="hero">
@@ -9,31 +8,75 @@ document.addEventListener("DOMContentLoaded",()=>{
       </div>`
 })
 
+async function animatePages() {
+  return Promise.all([
+    gsap.to("#animate1 div", {
+      height: "100%",
+      duration: 0.3,
+      stagger: 0.1,
+      onStart : ()=>{
+        document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+      }
+    }).then(),
+
+    gsap.to("#animate2 div", {
+      height: "100%",
+      duration: 0.3,
+      stagger: 0.1
+    }).then()
+  ]);
+}
+
+function animateOutPages() {
+  return Promise.all([
+    gsap.to("#animate1 div", {
+      height: "0%",
+      duration: 0.3,
+      stagger: 0.1
+    }).then(),
+
+    gsap.to("#animate2 div", {
+      height: "0%",
+      duration: 0.3,
+      stagger: 0.1,
+    }).then(),
+  ]);
+  
+}
+
+
 links.forEach((tab)=>{
     tab.addEventListener("click",async (dets)=>{
-
+        dets.preventDefault();
         // Add the gsap animation start here and await it 
-
+        await animatePages();
+        // Content get chnaged according to the links
         if(tab.textContent === "Home"){
             mainContent.innerHTML = `<div id="hero">
         <h1>Home</h1>
       </div>`
         }
-        else if(tab.textContent === "About"){
+        else if(tab.textContent === "Franchises"){
             mainContent.innerHTML = `<div id="hero">
-        <h1>About</h1>
+        <h1>Franchises</h1>
       </div>`
         }
-        else if(tab.textContent === "Contact"){
+        else if(tab.textContent === "Updates"){
             mainContent.innerHTML = `<div id="hero">
-        <h1>Contact</h1>
+        <h1>Updates</h1>
+      </div>`
+        }
+        else if(tab.textContent === "Soldiers"){
+            mainContent.innerHTML = `<div id="hero">
+        <h1>Soldiers</h1>
       </div>`
         }
         else{
             // dynamic creation of not found page...and push to the html (not implemented yet)
         }
-
+        await new Promise(r=>setTimeout(r,500));
         // Add the gsap animation of end here and await it
-        
+        await animateOutPages();
+        document.getElementsByTagName("body")[0].style.overflowY = "visible"
     })
 })
