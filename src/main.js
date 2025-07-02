99d078e32fb8dd55777af07c9e444a14f0f0da31
Kashyap Das <kashyap.education2234@gmail.com>
2025-07-02 23:26:59 +0530

    create the hero section with page transition

--- src/main.js
+++ src/main.js
@@ -3,9 +3,61 @@ const mainContent = document.querySelector("#contentPage");
 // Default page loaded on the website load for the 1st time
 document.addEventListener("DOMContentLoaded",()=>{
     // first time render the Home page
-    mainContent.innerHTML = `<div id="hero">
-        <h1>Home</h1>
-      </div>`
+    mainContent.innerHTML = `<div id="heading1">
+            <h1>Call of duty</h1>
+            <h1>SQUAD READY.</h1>
+            <h1>LET’S GO</h1>
+          </div>
+          
+          <div id="subheading">
+            <h1 id="homeBtn"><u>Fresh Loadouts</u></h1>
+            <h1 id="allMission">All Missions</h1>
+          </div>
+
+         <div class="scroll">
+            <div class="scroll-track1">
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+
+    <!-- Duplicated for seamless loop -->
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+            </div>
+         </div>
+         <div class="scroll">
+            <div class="scroll-track2">
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+
+    <!-- Duplicated for seamless loop -->
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+            </div>
+         </div>`
 })
 
 async function animatePages() {
@@ -27,7 +79,7 @@ async function animatePages() {
   ]);
 }
 
-function animateOutPages() {
+async function animateOutPages() {
   return Promise.all([
     gsap.to("#animate1 div", {
       height: "0%",
@@ -52,9 +104,61 @@ links.forEach((tab)=>{
         await animatePages();
         // Content get chnaged according to the links
         if(tab.textContent === "Home"){
-            mainContent.innerHTML = `<div id="hero">
-        <h1>Home</h1>
-      </div>`
+            mainContent.innerHTML = `<div id="heading1">
+            <h1>Call of duty</h1>
+            <h1>SQUAD READY.</h1>
+            <h1>LET’S GO</h1>
+          </div>
+          
+          <div id="subheading">
+            <h1 id="homeBtn"><u>Fresh Loadouts</u></h1>
+            <h1 id="allMission">All Missions</h1>
+          </div>
+
+         <div class="scroll">
+            <div class="scroll-track1">
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+
+    <!-- Duplicated for seamless loop -->
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+            </div>
+         </div>
+         <div class="scroll">
+            <div class="scroll-track2">
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+
+    <!-- Duplicated for seamless loop -->
+    <img src="./images/image1.jpg" />
+    <img src="./images/image2.jpg" />
+    <img src="./images/image3.jpg" />
+    <img src="./images/image4.jpg" />
+    <img src="./images/image5.jpg" />
+    <img src="./images/image6.jpg" />
+    <img src="./images/image7.jpg" />
+    <img src="./images/image8.jpg" />
+            </div>
+         </div>`
         }
         else if(tab.textContent === "Franchises"){
             mainContent.innerHTML = `<div id="hero">
